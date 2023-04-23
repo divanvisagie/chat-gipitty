9de0a170681b9a6a045f2bd945cd7f4d247a53db
Divan visagie <me@dvisagie.com>
2023-04-23 11:02:52 +0200

    Init window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+struct AppState {}
+
+impl Default for AppState {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
+impl eframe::App for AppState {
+    fn update(&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {}
+}
 fn main() {
     println!("Hello, world!");
+
+    //init eframe window
+    let app = eframe::NativeOptions::default();
+    match eframe::run_native(
+        "Chat Gipity",
+        app,
+        Box::new(|_cc| Box::<AppState>::default()),
+    ) {
+        Ok(_) => {}
+        Err(e) => {
+            println!("Error: {}", e);
+        }
+    }
 }
