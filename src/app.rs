b332a60de6e91bc912a611692233021fb8faf998
Divan visagie <me@dvisagie.com>
2023-04-23 17:29:43 +0200

    Add Grid List

--- src/app.rs
+++ src/app.rs
@@ -1,18 +1,24 @@
- fn test() {
-    println!("test");
-}
+use std::collections::HashMap;
+
 pub struct ChatEntry {
-    name: String,
-    message: String,
+    pub name: String,
+    pub message: String,
 }
 pub struct AppState {
     counter: i32,
+    chat_tree: HashMap<String, ChatEntry>,
 }
 
 impl Default for AppState {
     fn default() -> Self {
+        let mut chat_tree = HashMap::new();
+        chat_tree.insert("test".to_string(), ChatEntry {
+            name: "test".to_string(),
+            message: "test".to_string(),
+        });
         Self {
             counter: 0,
+            chat_tree: HashMap::new(),
         }
     }
 }
@@ -20,8 +26,11 @@ impl Default for AppState {
 impl eframe::App for AppState {
     fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
         eframe::egui::SidePanel::left("side-panel").show(ctx, |ui|{
-            ui.with_layout(eframe::egui::Layout::bottom_up(eframe::egui::Align::Center), |ui| {
+            ui.with_layout(eframe::egui::Layout::top_down(eframe::egui::Align::Center), |ui| {
                 ui.heading("Left Panel");
+                eframe::egui::Grid::new("my_grid").striped(true).show(ui, |ui| {
+                    ui.label("Label");
+                });
                 ui.add(eframe::egui::widgets::Button::new("Button"));
             }); 
         }); 
