86d181452f0d9336aebc321a217fe5066e23d97b
Anna Smith <annaleighsmith60@gmail.com>
2024-05-10 21:40:37 -0400

    final small edits

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::str::FromStr;
 
-use args::{Args, SubCommands, ConfigSubCommand};
+use args::{Args, ConfigSubCommand, SubCommands};
 use chatgpt::{GptClient, Message, Role};
 use clap::Parser;
 use cli::run;
-use utils::{get_file_contents_from_path, get_stdin, markdown_from_messages, is_valid_yaml};
+use utils::{get_file_contents_from_path, get_stdin, is_valid_yaml, markdown_from_messages};
 
 mod args;
 mod chatgpt;
@@ -12,15 +12,13 @@ mod cli;
 mod utils;
 
 fn main() {
-
     let mut client = GptClient::new();
     let args = Args::parse();
 
-    if let Some(SubCommands::Config(config_sc)) = &args.subcmd{
+    if let Some(SubCommands::Config(config_sc)) = &args.subcmd {
         handle_config_subcommand(&mut client, config_sc);
         return;
     }
-
 
     let stdin_text = get_stdin();
     if !stdin_text.is_empty() {
@@ -35,8 +33,14 @@ fn main() {
         }
     }
 
-    if let Some(_) = args.subcmd { // If view mode
-        let visible_messages = client.messages.iter().cloned().filter(|msg| msg.role != "system").collect();      
+    if let Some(_) = args.subcmd {
+        // If view mode
+        let visible_messages = client
+            .messages
+            .iter()
+            .cloned()
+            .filter(|msg| msg.role != "system")
+            .collect();
         let md = markdown_from_messages(visible_messages);
         println!("{}", md);
         return;
@@ -58,11 +62,13 @@ fn handle_config_subcommand(client: &mut chatgpt::GptClient, config_sc: &ConfigS
         let parts: Vec<&str> = set.split('=').collect();
         if parts.len() == 2 {
             client.set_config_value(parts[0], parts[1]);
-            println!("Configuration set successfully for {} to {}", parts[0], parts[1])
+            println!(
+                "Configuration set successfully for {} to {}",
+                parts[0], parts[1]
+            )
         } else {
             println!("Invalid format for setting configuration. Use key=value");
         }
-        // Additional logic to set the configuration
     }
     if let Some(ref get) = config_sc.get {
         let value = client.get_config_value(get);
